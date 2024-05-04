78b42e012c64b901c3ca0998bdf22865faff90fe
boham97 <kangbosung97@gmail.com>
2024-05-04 18:20:49 +0900

    chat-room-service 셋팅

---
+++ chat-room-service/src/main/java/hello/hellospring/ChatRoomApplication.java
@@ -0,0 +1,15 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ChatRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRoomApplication.class, args);
+	}
+
+}
